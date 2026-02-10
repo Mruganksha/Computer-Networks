f4ea5f79e245cde16ba761505744f34f5497d515
Mruganksha <kudakemruganksha@gmail.com>
2026-02-10 17:35:31 +0530

    modified client server mail

--- mail_client.py
+++ mail_client.py
@@ -1,17 +1,38 @@
+
 import smtplib
 
-sender_email = input("Enter your email: ")
-password = input("Enter your password: ")
+receiver_email = input("Enter recipient email: ")
+message = input("Enter your message: ")
+
+sender_email = "client@localhost"
+
+server = smtplib.SMTP("localhost", 1025)
+
+server.sendmail(sender_email, receiver_email, message)
+
+server.quit()
+
+print("Mail transferred successfully!")
+
+#using goggle service
+'''
+import smtplib
+
+sender_email = "kudakemruganksha30@gmail.com"        
+sender_password = "mibg fmug hmep twqz"   
+
 receiver_email = input("Enter recipient email: ")
 message = input("Enter your message: ")
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()  
 
-server.login(sender_email, password)
+server.login(sender_email, sender_password)
 
 server.sendmail(sender_email, receiver_email, message)
 
 server.quit()
 
 print("Email sent successfully!")
+'''
+
